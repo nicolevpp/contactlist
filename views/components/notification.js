ca08495a5cf069fa651dcc82ace56fc9214fbd2a
Nicole Perez <nicoleperezdigital@gmail.com>
2023-09-10 17:06:16 -0400

    change style notification

--- views/components/notification.js
+++ views/components/notification.js
@@ -11,7 +11,7 @@ export const createNotification = (isError, message) => {
     } else {
     div.innerHTML = `
         <div class="max-w-7xl mx-auto px-4 flex justify-end">
-            <p class=" bg-green-500 p-4 w-3/12 rounded-lg font-bold">${message}</p>
+            <p class=" bg-green-500 p-4 w-3/12 rounded-lg font-bold shadow-md">${message}</p>
         </div>
     `
 }
